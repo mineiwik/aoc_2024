bcd535aeb157e5d7c5c56af4eebd98950e6aa0fe
mineiwik <29404520+mineiwik@users.noreply.github.com>
2024-12-03 06:27:00 +0100

    day 3 finished & add day template

--- src/days/d3.rs
+++ src/days/d3.rs
@@ -1,14 +1,38 @@
+use regex::Regex;
+
 use crate::utils::DaySolver;
 
 pub struct Day3;
 
 impl DaySolver for Day3 {
-    fn part1(&self, _input: &str) -> Option<String> {
-        None
+    fn part1(&self, input: &str) -> Option<String> {
+        let mul_re = Regex::new(r"(mul\(\d{1,3},\d{1,3}\))").unwrap();
+        let op_re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+        let mut sum = 0;
+        for (_, [mul]) in mul_re.captures_iter(input).map(|c| c.extract()) {
+            let c = op_re.captures(mul).unwrap();
+            sum += c[1].parse::<isize>().unwrap() * c[2].parse::<isize>().unwrap();
+        }
+        Some(sum.to_string())
     }
 
-    fn part2(&self, _input: &str) -> Option<String> {
-        None
+    fn part2(&self, input: &str) -> Option<String> {
+        let mul_re = Regex::new(r"(mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\))").unwrap();
+        let op_re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+        let mut sum = 0;
+        let mut mul_enabled = true;
+        for (_, [mul]) in mul_re.captures_iter(input).map(|c| c.extract()) {
+            match mul {
+                "don't()" => mul_enabled = false,
+                "do()" => mul_enabled = true,
+                _ if mul_enabled => {
+                    let c = op_re.captures(mul).unwrap();
+                    sum += c[1].parse::<isize>().unwrap() * c[2].parse::<isize>().unwrap();
+                }
+                _ => {}
+            }
+        }
+        Some(sum.to_string())
     }
 }
 
@@ -19,11 +43,14 @@ mod tests {
 
     #[test]
     fn sample() {
-        let _input = indoc! {"
-            TODO
+        let input1 = indoc! {"
+            xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
         "};
-        let _solver = Day3 {};
-        //assert_eq!(solver.part1(input).unwrap(), "TODO");
-        //assert_eq!(solver.part2(input).unwrap(), "TODO");
+        let input2 = indoc! {"
+            xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+        "};
+        let solver = Day3 {};
+        assert_eq!(solver.part1(input1).unwrap(), "161");
+        assert_eq!(solver.part2(input2).unwrap(), "48");
     }
 }
